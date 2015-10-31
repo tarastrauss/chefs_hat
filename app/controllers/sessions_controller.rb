16cd2e8a91ef2ae95f95e6fb69e714d9a9299bf4
Tara Strauss <tara.m.strauss@gmail.com>
2015-10-31 15:29:50 -0700

    adds routes and forms for user sign up

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Loggied in!'
+    else
+      flash.now.alert = 'Invalid login credentials - try again!'
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "logged out!"
+  end
+end
